dd5f63af77cbfee4ce638b5e6b16ce74c95b15a9
chris <caherdev@gmail.com>
2025-09-20 13:32:32 -0600

    solving two sum using unordered_map

--- easy/two_sum.cpp
+++ easy/two_sum.cpp
@@ -1,21 +1,36 @@
 #include<iostream>
 #include<vector>
+#include<unordered_map>
 #define REP(i,a,b) for(int i = a ; i<b; i++);
 
 using namespace std;
 
 typedef vector<int> vi;
 
-int main(){
-    vi n = {3,2,4};
-    int target = 6;
+//Two sum algorithm implementing hash map
 
-    for(int i = 0; i < n.size(); i++){
-        for(int j = i+1; j < n.size(); j++){
-            if(n[i] +n[j] == target)
-                cout << i << j << "\n";
+vi twoSum( const vi &nums, int target){
+    unordered_map<int, int> seen;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int complement = target - nums[i];
+        if (seen.find(complement) != seen.end()){
+            return {seen[complement], i};
         }
+        seen[nums[i]] = i;
+        
+    }
+    return {};
+}
+
+int main(){
+    vi nums = {2,7,11,15};
+    int target = 18;
+    vi result = twoSum(nums, target);
+    if(!result.empty()){
+        cout << result[0] << " " << result[1];
+    }else{
+        cout << "No solution";
     }
     return 0;
-
 }
