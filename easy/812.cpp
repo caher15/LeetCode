fa78f66da9e7e4d854aa64fa37a10ffcd457398d
chris <caherdev@gmail.com>
2025-09-28 16:27:35 -0600

    Largest Triangle Area second method

--- easy/812.cpp
+++ easy/812.cpp
@@ -1,5 +1,5 @@
 
-//812. Largest Triangle Area
+//812. Largest Triangle Area second method
 // Given an array of points on the X-Y plane points where points[i] = [xi, yi]
 // return the area of the largest triangle that 
 // can be formed by any three different points
@@ -9,25 +9,22 @@
 
 using namespace std;
 
-double largestTriangleArea(vector<vector<int>>& points) {
-    const int n=points.size();
-    double maxArea=0;
-    for(int i=0; i<n-2; i++){
-        const int a0=points[i][0], a1=points[i][1];
-        for(int j=i+1; j<n-1; j++){
-            for(int k=j+1; k<n; k++){
-                double area=0.5*((points[j][0]-a0)*(points[k][1]-a1)-(points[j][1]-a1)*(points[k][0]-a0));
-                if (area<0) area=-area;
-                maxArea=max(maxArea, area);
-
-            }
-        }
+    double largestTriangleArea(vector<vector<int>>&p) {
+        double res = 0;
+        int n = p.size();
+        for(int i=0; i<n-2; i++)
+            for(int j=i+1; j<n-1; j++)
+                for(int k=j+1; k<n; k++)
+                {
+                    double area = abs( (p[j][0]-p[i][0]) * (p[k][1]-p[i][1]) - (p[j][1] - p[i][1]) * (p[k][0] - p[i][0]) ) * 0.5;
+                    res = max(res, area);
+                }
+        return res;
     }
-    return maxArea;
-}
+
 
 int main(){
-    vector<vector<int>> points = {{0,0},{0,1},{1,0},{0,2},{2,0}};
+    vector<vector<int>> points = {{0,0},{0,1},{1,0},{1,2}};
     cout << largestTriangleArea(points);
     return 0;
 }
